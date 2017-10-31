cded0a5442caab8cf4eed261869e79d9a219a8eb
Allegra Larche <alarche@seas.upenn.edu>
2017-10-30 21:56:57 -0400

    set up ui proxy and added core components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DiscoverComponent } from './discover/discover.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    DiscoverComponent,
+    NewsfeedComponent
   ],
   imports: [
     BrowserModule
