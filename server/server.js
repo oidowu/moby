11ca9cf4a18ebbdd4ad693542c8ca709c18f932a
Tobi Idowu <tidowu93@gmail.com>
2017-11-12 14:53:24 -0500

    Sample FeedParsing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var request = require('request');
+var FeedParser = require('feedparser');
 
 var app = module.exports = loopback();
 
@@ -15,6 +17,7 @@ app.start = function() {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
+    fetch('http://feeds.washingtonpost.com/rss/politics')
   });
 };
 
@@ -22,8 +25,47 @@ app.start = function() {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
-
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
 });
+
+function fetch(feed) {
+  var req = request(feed, {timeout: 10000, pool: false});
+  req.setMaxListeners(50);
+  var feedparser = new FeedParser();
+  req.on('error', done);
+  req.on('response', function(res) {
+    if (res.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+    var encoding = res.headers['content-encoding'] || 'identity';
+    var charset = getParams(res.headers['content-type'] || '').charset;
+    res.pipe(feedparser);
+  });
+  feedparser.on('error', done);
+  feedparser.on('end', done);
+  feedparser.on('readable', function() {
+    var post;
+    while (post = this.read()) {
+      console.log(post);
+    }
+  });
+}
+
+function getParams(str) {
+  var params = str.split(';').reduce(function (params, param) {
+    var parts = param.split('=').map(function (part) { return part.trim(); });
+    if (parts.length === 2) {
+      params[parts[0]] = parts[1];
+    }
+    return params;
+  }, {});
+  return params;
+}
+
+function done(err) {
+  if (err) {
+    console.log(err, err.stack);
+    return process.exit(1);
+  }
+  process.exit();
+}
